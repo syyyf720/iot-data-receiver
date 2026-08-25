f90bdadd31234a570f7d7a71700aa56a48549ad3
syyyf <2415165418@qq.com>
2026-08-25 18:00:51 +0800

    新增 ECharts 图表页面和 /api/chart_data 接口

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 from models import SensorData
 from config import Session
 
@@ -9,10 +9,10 @@ app = Flask(__name__)
 def upload():
     temp = request.json.get('temperature')
     humi = request.json.get('humidity')
-    locals = request.json.get('location')
+    localtion = request.json.get('location')
     
     session = Session()
-    data = SensorData(temperature=temp, humidity=humi, location=locals)
+    data = SensorData(temperature=temp, humidity=humi, location=localtion)
     session.add(data)
     session.commit()
     session.close()
@@ -29,8 +29,23 @@ def get_data():
         'id': d.id,
         'temp': d.temperature,
         'humi': d.humidity,
-        'locals': d.location
+        'localtion': d.location
     } for d in all_data])
+
+@app.route('/api/chart_data')
+def chart_data():
+    session = Session()
+    all_data = session.query(SensorData).order_by(SensorData.id).all()
+    session.close()
+    temp_data = [{'id':d.id,'value':d.temperature} for d in all_data]
+    humi_data = [{'id':d.id,'value':d.humidity} for d in all_data]
+    return jsonify({'temp' : temp_data,'humi' : humi_data})
+
+@app.route('/chart')
+def chart_page():
+    return render_template('chart.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
